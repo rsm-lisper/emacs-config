3fcc9dc797cd6bab5a67793c060a4a7081baee28
rsm-lisper <rsm@disroot.org>
2023-05-08 21:09:24 +0200

    Porządki i aktualizacja konfiguracji.

--- init.el
+++ init.el
@@ -1,4 +1,13 @@
 
+;;; Code:
+
+;; dodatkowe repo pakietów - nongnu i melpa
+(require 'package)
+;(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/packages/"))
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;;(package-initialize)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -7,7 +16,7 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(custom-safe-themes
-   '("9fad628c15f1e94af44e07b00ebe3c15109be28f4d73adf4a9e22090845cbce9" default))
+   '("82225f1fa1e4d3b00c63700f691fc0dc7c9bdab8a996e6a78f451f9a15bd74fc" "7922b14d8971cce37ddb5e487dbc18da5444c47f766178e5a4e72f90437c0711" "9fad628c15f1e94af44e07b00ebe3c15109be28f4d73adf4a9e22090845cbce9" default))
  '(doc-view-continuous t)
  '(geiser-guile-warning-level 'high)
  '(global-linum-mode t)
@@ -16,6 +25,8 @@
  '(inhibit-startup-screen t)
  '(ispell-dictionary nil)
  '(make-backup-files nil)
+ '(package-selected-packages
+   '(tangotango-theme rainbow-blocks flycheck rainbow-identifiers rainbow-delimiters dired-rainbow material-theme leuven-theme diff-hl geiser-guile))
  '(tool-bar-mode nil))
 
 (custom-set-faces
@@ -25,4 +36,23 @@
  ;; If there is more than one, they won't work right.
  )
 
-(load-theme 'tango-dark)
+(load-theme 'material)
+(load-theme 'tangotango)
+
+;;;; diff highlight - oznaczanie zmian na podstawie repo (poprzez vc więc obsługuje różne)
+;;(require-package 'diff-hl)
+(require 'diff-hl)
+(global-diff-hl-mode)   ; włącza diff-hl dla wszystkich buforów
+(diff-hl-flydiff-mode)  ; włącza diff w locie - nie trzeba zapisywać pliku żeby widział zmiany
+(add-hook 'dired-mode-hook 'diff-hl-dired-mode)  ; dodaje diff-hl w dired
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook #'rainbow-identifiers-mode)
+
+;;;; guile
+(setenv "GUILE_LOAD_PATH"
+	(concat (getenv "HOME") "/proj:" (getenv "HOME") "/guile-libs"))
+
+;;; init.el ends here
