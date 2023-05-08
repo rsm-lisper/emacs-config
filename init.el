accaadd3353e5ea057ace974bf66df6b9f1ced29
rsm-lisper <rsm@disroot.org>
2023-05-08 21:07:34 +0200

    Porządki i aktualizacja konfiguracji.

--- init.el
+++ init.el
@@ -1,27 +1,28 @@
 
-(load "~/.emacs.d/common.el")
 
-;;; - koniec mojego init.el -
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(custom-safe-themes
-   '("43cadc6254cf27ff544e044b4139a7d50cf44e107cffef255aa8c5943581f606" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "170bb47b35baa3d2439f0fd26b49f4278e9a8decf611aa33a0dad1397620ddc3" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "a77ced882e25028e994d168a612c763a4feb8c4ab67c5ff48688654d0264370c" "13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "713f898dd8c881c139b62cf05b7ac476d05735825d49006255c0a31f9a4f46ab" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" default))
- '(display-time-mode t)
+   '("9fad628c15f1e94af44e07b00ebe3c15109be28f4d73adf4a9e22090845cbce9" default))
  '(doc-view-continuous t)
- '(fringe-mode 4 nil (fringe))
  '(geiser-guile-warning-level 'high)
  '(global-linum-mode t)
+ '(indicate-buffer-boundaries 'left)
+ '(indicate-empty-lines 1)
+ '(inhibit-startup-screen t)
+ '(ispell-dictionary nil)
  '(make-backup-files nil)
- '(package-selected-packages
-   '(tangotango-theme slime ox-gfm moe-theme leuven-theme gh-md geiser diff-hl))
- '(ring-bell-function 'ignore)
  '(tool-bar-mode nil))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
+ )
+
+(load-theme 'tango-dark)
