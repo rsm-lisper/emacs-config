367f9020dad97782dfa1ab3b5d90cabd1ada87b7
rsm-lisper <rsm@disroot.org>
2023-05-09 17:41:45 +0200

    Poprawka w instalacji.

--- install.el
+++ install.el
@@ -5,6 +5,8 @@
 
 (defvar my-packages '(material-theme tangotango-theme diff-hl flycheck rainbow-delimiters rainbow-identifiers))
 
+(package-refresh-contents)
+
 (mapcar (lambda (p)
 	  (package-install p)
 	  (require p))
